bfcdf399bc7157e220eb7e9d5b4acfdffd955970
Bui Tuan <buianhtuan657@gmail.com>
2021-03-10 11:13:50 +0700

    created_project

---
+++ src/test/java/com/javazero/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javazero.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
